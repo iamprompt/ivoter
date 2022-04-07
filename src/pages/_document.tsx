76f468168a26531a1f77c7ae7389826944d65a57
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-04-07 20:28:05 +0700

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import type { DocumentContext } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300;400;700&family=IBM+Plex+Sans+Thai+Looped:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default _Document
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['IBM Plex Sans Thai', 'system-ui', 'sans-serif'],
+      serif: ['IBM Plex Sans Thai Looped', 'system-ui', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
